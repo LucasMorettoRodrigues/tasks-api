8c4900c0e0bd7b4342f246254e0f852d9469acee
Lucas <r.lucasmoretto@gmail.com>
2022-01-30 17:13:23 -0300

    add update controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -24,7 +24,7 @@ const getTask = async (req, res) => {
         const task = await Task.findOne({ _id: taskID })
 
         if(!task) {
-            return res.status(404).json(`Not found task with id ${taskID}`)
+            return res.status(404).json({ msg: `No task with id: ${taskID}`})
         }
 
         res.send({ task })
@@ -33,8 +33,22 @@ const getTask = async (req, res) => {
     }
 }
 
-const updateTask = (req, res) => {
-    res.send('update task')
+const updateTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params
+        const newTask = req.body
+        const task = await Task.findOneAndUpdate({ _id: taskID }, newTask, { 
+            new: true,
+            runValidators: true })
+        
+        if(!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}`})
+        }
+
+        res.status(200).json({ task })
+    } catch (error) {
+        res.status(500).json({ error })
+    }
 }
 
 const deleteTask = (req, res) => {
